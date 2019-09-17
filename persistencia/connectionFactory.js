ef2d82e9ed62b6792c05b61b4e61f90b26877a24
Jean Jacques Nascimento Barros <jjean.jacques10@gmail.com>
2019-09-17 17:48:28 -0300

    Update Connection

--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -2,9 +2,9 @@ var mysql  = require('mysql');
 
 function createDBConnection(){
 		return mysql.createConnection({
-			host: '172.20.0.2',
-			user: 'docker',
-			password: 'banana pijama',
+			host: '',
+			user: '',
+			password: '',
 			database: 'payfast'
 		});
 }
