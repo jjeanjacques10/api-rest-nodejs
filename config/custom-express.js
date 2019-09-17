b08aad837a5f437187ed16ffb69a2e410a2887a3
Jean Jacques <jean@okn.com.br>
2019-09-17 17:40:31 -0300

    Continuação do curso 2

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
-module.exports = function(){
+module.exports = function () {
     var app = express();
 
-    app.use(bodyParser.urlencoded({extend: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    
+
+    //Obrigatoriamente logo apos o bodyParser
+    app.use(expressValidator());
 
     //adicionar a pasta controllers no app
     consign()
         .include('controllers')
+        .then('persistencia')
         .into(app);
 
     return app;
